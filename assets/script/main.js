b0c9d2125f04c6d5155a11bef72859e5f23c4225
Laila Ali <ms.laila.ali@outlook.com>
2017-11-11 02:33:08 -0500

    connected displays and buttons w/js

--- assets/script/main.js
+++ assets/script/main.js
@@ -17,6 +17,9 @@ let player = {
     },
     healCount: 0,
 }
+var playerHp = document.getElementById("player-hp-display");
+var displayCurrentEvent = document.getElementById("display-text");
+
 
 
 function getPlayerDamage() {
@@ -27,78 +30,87 @@ function getDamage() {
     return getRandomIntInclusive(1, 5);
 }
 
-function playerAttack () {
+function playerAttack() {
     grant.health = grant.health - player.attack();
-
 }
+
 
 function executeRound() {
     console.log("player Health", player.health, "grant health", grant.health, "player wins", player.wins, "player heal count", player.healCount, );
-/*     } else if (userAnswer === "heal") {
-        player.heal();
-    }
-
-    else {
-        return;
-    } */
-
+    
     player.health = player.health - grant.attack();
     if (player.health <= 0) {
+        displayCurrentEvent.innerHTML = (player.name + ", my brave hero, you've lost!");
         console.log("you lost!");
         return;
     }
+
     if (grant.health <= 0) {
         grant.health = 10;
         player.wins = player.wins + 1
         var roundsLeft = 3 - player.wins;
 
+        //Update Displays
         var roundDisplay = document.getElementById("display-round");
-        roundDisplay.innerHTML = ("<p>" + roundsLeft + " Round(s) Left</p>");
+        roundDisplay.innerHTML = ("<p>" + roundsLeft + " Round(s) Left</p>");       //Displays how many rounds are left      
 
         var grantsLosses = document.getElementById("grant-losses-count");
-        grantsLosses.innerHTML = (player.wins);
+        grantsLosses.innerHTML = (player.wins);                                                            //Displays how many times Grant lost
 
         var playerWins = document.getElementById("player-wins-count");
-        playerWins.innerHTML = (player.wins);
+        playerWins.innerHTML = (player.wins);                                                               //Displays how many times player won
+    }
 
-    }
-    if (player.wins >= 3) {
+    if (player.wins >= 3 ) {
+        displayCurrentEvent.innerHTML = (player.name + ", my brave hero, you've won!");
         console.log("player wins")
-        return;
-    }
+    } 
 }
 
-
-
 function startGame() {
-
-    // User is prompted to play the game
-
     var start = prompt("Do you want to play?");
-
-    console.log(start);
 
     // If yes, user is prompted to enter their name
     if (start === "yes") {
         player.name = prompt("Enter your name");
         console.log(player.name);
 
-
     } else {
         console.log("quit");
     }
-    //If no, quit
 };
 
-
+//Start Game Button Function
 var startBtn = document.getElementById("start-game");
 startBtn.onclick = startGame;
 
+//Attack Button Function
 var attackBtn = document.getElementById("attack");
 attackBtn.onclick = function () {
     playerAttack();
     executeRound();
+    playerHp.textContent = (player.health);         //Displays player's health points
+    var grantHealth = document.getElementById("grant-hp-display");
+    grantHealth.textContent = (grant.health);           //Displays Grant's health points
+    displayCurrentEvent.innerHTML = (player.name + ", Grant attacked you! What would you like to do?");
 }
+
+//Heal Button Function
+var healBtn = document.getElementById("heal");
+healBtn.onclick = function () {
+    player.heal();
+    playerHp.textContent = (player.health);         //Displays player's health points
+    var playerHealthCountUsed = document.getElementById("player-hc-display");
+    playerHealthCountUsed.textContent = (player.healCount = 2 - player.healCount);         //Displays player's health count left
+}
+
+//Flee Button Function
+var fleeBtn = document.getElementById("quit");
+fleeBtn.onclick = function () {
+    displayCurrentEvent.innerHTML = ("You fled! Try again when you are better prepared!");
+    return;
+}
+
 
 
 
@@ -107,13 +119,3 @@ function getRandomIntInclusive(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
-
-
-
-
-
-
-
-
-
-
